6b250849964539acef5e0864c40c462f23090cdf
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-04-24 18:49:05 +0000

    added first webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Hai Tran
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+/**
+ * This function displays the slider value.
+ */
+function generateRandomNumber() {
+  const randomNumber = Math.floor(Math.random() * 6) + 1
+
+  const optionPositive = document.getElementById('option-positive').checked
+
+  if (optionPositive == true) {
+    document.getElementById("result").innerHTML = "The random number is : " + randomNumber
+  } else {
+    document.getElementById("result").innerHTML = "The random number is : -" + randomNumber
+  }
+}
